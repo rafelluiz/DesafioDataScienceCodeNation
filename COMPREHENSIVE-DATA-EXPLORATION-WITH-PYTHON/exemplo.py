37f253f2bd760904a1fd4dda072c87c007047049
Rafael Luiz de Araújo Oliveira <rafelluiz@gmail.com>
2020-06-03 15:55:43 -0300

    Não consegui acompanhar e adaptar o artigo ao meus dados, vou procurar outros materias.

--- COMPREHENSIVE-DATA-EXPLORATION-WITH-PYTHON/exemplo.py
+++ COMPREHENSIVE-DATA-EXPLORATION-WITH-PYTHON/exemplo.py
@@ -7,7 +7,17 @@ from sklearn.preprocessing import StandardScaler
 from scipy import stats
 import warnings
 warnings.filterwarnings('ignore')
+#%matplotlib inline
 
 df_train = pd.read_csv('../TEST_FILES/train.csv')
 
 df_train.columns
+
+print(df_train['NU_NOTA_MT'].describe())
+
+sns.distplot(df_train['NU_NOTA_MT'])
+
+print("Skewness: %f" % df_train['NU_NOTA_MT'].skew())
+print("Kurtosis: %f " % df_train['NU_NOTA_MT'].kurt())
+
+plt.show()
