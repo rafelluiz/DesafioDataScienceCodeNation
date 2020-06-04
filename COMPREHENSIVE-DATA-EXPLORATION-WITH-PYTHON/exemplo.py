321cc95010a60c7846a0d8b3fe08311401ad1b08
Rafael Luiz de Araújo Oliveira <rafelluiz@gmail.com>
2020-06-04 17:11:47 -0300

    Concluisão do exemplo ano de construção das casas.

--- COMPREHENSIVE-DATA-EXPLORATION-WITH-PYTHON/exemplo.py
+++ COMPREHENSIVE-DATA-EXPLORATION-WITH-PYTHON/exemplo.py
@@ -1,3 +1,5 @@
+# https://www.kaggle.com/pmarcelino/comprehensive-data-exploration-with-python?select=data_description.txt
+
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -25,4 +27,22 @@ print("Kurtosis: %f " % df_train['SalePrice'].kurt())
 var = 'GrLivArea'
 data = pd.concat([df_train['SalePrice'],df_train[var]],axis=1)
 data.plot.scatter(x=var,y='SalePrice', ylim=(0,800000))
+
+var = 'TotalBsmtSF'
+data = pd.concat([df_train['SalePrice'],df_train[var]],axis=1)
+data.plot.scatter(x=var,y='SalePrice',ylim=(0,800000))
+
+var = 'OverallQual'
+data = pd.concat([df_train['SalePrice'],df_train[var]], axis=1)
+f,ax = plt.subplots(figsize = (8, 6))
+fig = sns.boxenplot(x=var, y="SalePrice",data=data)
+fig.axis(ymin=0,ymax=800000)
+
+var = 'YearBuilt'
+data = pd.concat([df_train['SalePrice'],df_train[var]], axis=1)
+f, ax = plt.subplots(figsize=(16,8))
+fig = sns.boxplot(x=var, y="SalePrice", data=data)
+fig.axis(ymin=0, ymax=800000)
+plt.xticks(rotation=90)
+
 plt.show()
