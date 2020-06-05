7f4bae6a98344499d1981682f570b18a30748ee8
Rafael Luiz de Araújo Oliveira <rafelluiz@gmail.com>
2020-06-05 03:54:33 -0300

    Gráfico de Dispersão.

--- COMPREHENSIVE-DATA-EXPLORATION-WITH-PYTHON/exemplo.py
+++ COMPREHENSIVE-DATA-EXPLORATION-WITH-PYTHON/exemplo.py
@@ -56,4 +56,9 @@ sns.set(font_scale=1.25)
 hm = sns.heatmap(cm, cbar=True, annot=True, square=True, fmt='.2f', annot_kws={'size':10}, yticklabels=cols.values,
                  xticklabels=cols.values)
 
+sns.set()
+cols = ['SalePrice', 'OverallQual', 'GrLivArea', 'GarageCars' , 'TotalBsmtSF', 'FullBath', 'YearBuilt']
+sns.pairplot(df_train[cols], size= 2.5)
+
+
 plt.show()
