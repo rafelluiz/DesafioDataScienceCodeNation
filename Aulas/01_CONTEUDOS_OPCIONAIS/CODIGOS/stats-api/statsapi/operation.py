6bbd96d9398866464a6e4ab5ae018a5fee68a028
Rafael Luiz de Araújo Oliveira <rafelluiz@gmail.com>
2020-06-17 08:35:32 -0300

    Alteracao diretorio da pasta de codigos Foi alterado o diretorio das pastas para ficar mais consistente.

--- Aulas/01_CONTEUDOS_OPCIONAIS/CODIGOS/stats-api/statsapi/operation.py
+++ Aulas/01_CONTEUDOS_OPCIONAIS/CODIGOS/stats-api/statsapi/operation.py
@@ -1,5 +1,3 @@
-from loguru import logger
-
 def op_mean(data):
   return sum(data)/len(data)
 
@@ -7,7 +5,6 @@ def op_max(data):
   return max(data)
 
 def op_min(data):
-  logger.debug(f'data: {data}')
   return min(data)
 
 
