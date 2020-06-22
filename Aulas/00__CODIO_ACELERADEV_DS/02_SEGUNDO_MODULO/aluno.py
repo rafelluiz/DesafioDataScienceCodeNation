d15d2aacb688cfe56760de8ea64f0be7fc46c993
Rafael Luiz de Araújo Oliveira <rafelluiz@gmail.com>
2020-06-22 09:17:06 -0300

    Links das Aulas / Fixacao de exercicio

--- Aulas/00__CODIO_ACELERADEV_DS/02_SEGUNDO_MODULO/aluno.py
+++ Aulas/00__CODIO_ACELERADEV_DS/02_SEGUNDO_MODULO/aluno.py
@@ -2,6 +2,9 @@ import streamlit as st
 
 def main():
   st.title('Hello World')
+  st.header('This is a Header')
+  st.subheader('This is a subheader')
+  st.text('Isto é um texto')
 
 if __name__ == '__main__':
   main()
