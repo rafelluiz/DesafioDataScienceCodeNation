fe9e8c5f1220b39ce1a270f2049f740cbb4c653b
Rafael Luiz de Araújo Oliveira <rafelluiz@gmail.com>
2020-06-22 10:52:33 -0300

    Inserção de Funções do Pandas com StreamLit

--- Aulas/00__CODIO_ACELERADEV_DS/02_SEGUNDO_MODULO/StreamLit com Pandas.py
+++ Aulas/00__CODIO_ACELERADEV_DS/02_SEGUNDO_MODULO/StreamLit com Pandas.py
@@ -1,40 +1,20 @@
 import streamlit as st
+import pandas as pd
 
 def main():
-  st.title('Aula Streamlit')
-  st.markdown('# Botão')
-  botao = st.button('Botao')
+  st.title('Pandas com StreamLit')
+  st.image('logo.png')
+  file = 'IRIS.csv'
+  #file = st.file_uploader('Escolha o arquivo para upload',type='csv')
+  if file:
+    slider = st.slider('Valores',1,100)
+    df = pd.read_csv(file)
+    st.dataframe(df.head(slider))
 
-  if botao:
-    st.markdown('**Botão Clicado**')
+    st.markdown('Table')
+    st.table(df.head(slider))
+    st.write(df.columns)
 
-  st.markdown('Checkbox')
-
-  check = st.checkbox('CheckBox')
-  if check:
-    st.markdown('__Clicado__')
-
-  st.markdown('Radio:')
-  radio = st.radio('Escolha as opçoes',['Opt 1','Opt 2'])
-  if radio == 'Opt 1':
-    st.markdown('Opt 1')
-  else:
-    st.markdown('Opt 2')
-
-  st.markdown('Selectbox')
-  select = st.selectbox('Choose opt',['Opt 1','Opt 2'])
-  if select == 'Opt 1':
-    st.markdown('Opt 1')
-  else:
-    st.markdown('Opt 2')
-
-  st.markdown('MultSelect')
-  multi_select = st.multiselect('Chose: ',['Opt 1','Opt 2','Opt 3','Opt 4','Opt 5'])
-  if multi_select:
-    imprimir_multiselect = ''
-    for i in multi_select:
-      imprimir_multiselect += f' {i}'
-    st.markdown(imprimir_multiselect)
 
 
 if __name__ == '__main__':
